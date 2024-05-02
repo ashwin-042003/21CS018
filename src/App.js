bd36f303c47efd0765ea6ca8ca161c150904097d
ashwin-042003 <ashwin979149@gmail.com>
2024-05-02 12:37:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file if needed
+import AverageCalculator from './AverageCalculator'; // Import the AverageCalculator component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AverageCalculator /> {/* Render the AverageCalculator component */}
     </div>
   );
 }
